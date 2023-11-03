6977470e6f511acde5ea6c56d5228b024802ffe4
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-11-03 12:30:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import HomePage from './components/Home/Homepage';
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <>
-      <div>
-        <Navbar />
-        <HomePage />
-        <Footer />
-      </div>
+      <Navbar />
+
+      <Footer />
     </>
+
   );
 }
 
