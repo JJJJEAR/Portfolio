6c49cd13c0009f5e5739d5ad9cfebf6e676604fa
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-11-03 12:30:13 +0700

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function ResumeNew() {
+function Resume() {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
@@ -42,4 +42,4 @@ function ResumeNew() {
   );
 }
 
-export default ResumeNew;
+export default Resume;
