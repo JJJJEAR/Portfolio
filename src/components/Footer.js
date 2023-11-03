006b558f291417fe45e6fb8974b5480fb40ae3e2
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-11-03 12:30:08 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import Github from "@iconscout/react-unicons/icons/uil-github";
+import LinkedIn from "@iconscout/react-unicons/icons/uil-linkedin";
 
 function Footer() {
- 
+
   return (
     <Container fluid className="text-white p-4 bg-black" >
       <Row className="text-center ">
         <Col md={4}>
-          <h5 className="pt-3">Designed and Developed by Jullajak Saksri</h5>
+          <h5 className="pt-3">Designed and Developed by</h5><br />
+          <p >6303051623152 EnET-C<br /> Jullajak Saksri </p>
         </Col>
         <Col md={4}>
           <h5 className="pt-3">jullajak.jj@gmail.com</h5>
@@ -19,17 +21,17 @@ function Footer() {
           <ul className="list-inline social-icons">
             <li className="list-inline-item">
               <a
-                href="https://github.com/JJJEAR"
+                href="https://github.com/JJJJEAR"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Gitub color="white" size={"3rem"} />
+                <Github color="white" size={"3rem"} />
               </a>
             </li>
             <li className="list-inline-item">
               <a
-                href="https://twitter.com/"
+                href="https://www.facebook.com/jear.saksri/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -39,17 +41,17 @@ function Footer() {
             </li>
             <li className="list-inline-item">
               <a
-                href="https://www.linkedin.com/"
+                href="https://www.linkedin.com/in/jullajak-saksri/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Gitub color="white" size={"3rem"} />
+                <LinkedIn color="white" size={"3rem"} />
               </a>
             </li>
             <li className="list-inline-item">
               <a
-                href="https://www.instagram.com/"
+                href="https://www.instagram.com/jj.jear/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
